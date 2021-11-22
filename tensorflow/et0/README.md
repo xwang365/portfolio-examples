ef4f741cbd7ee87ba9ed949e736cec9e27b8842d
WangQiangItachi <761605677@qq.com>
2021-11-22 02:03:47 +0000

    add et0 example

---
+++ tensorflow/et0/README.md
@@ -0,0 +1,30 @@
+# Implementation of 2D Ordinary Kriging algorithm and calculate Reference Evapotranspiration(ET0) on IPU using TensorFlow
+1.This project is based on [PyKrige](https://github.com/GeoStat-Framework/PyKrige), we have implemented the `example/00_ordinary.py` example by using TensorFlow on IPU. You can achieve almost the same result.  
+2.ET0 is a common method for calculating meteorological elements, the calculation process of various ET0 is roughly the same, and kriging algorithm used is one of the more famous interpolation algorithms.  
+3.We do interpolation analysis of weather elements such as temperature, humidity, pressure and wind speed, and use Penman equation to calculate the ET0, which can get the law of water transpiration in the area. It's of great value for the research of meteorological data mining, evaluation of numerical weather forecasting, and climate change, etc.  
+4.The input form of ET0 is discrete points, the form of each point is [longitude, latitude, pressure/wind speed/temperature], and there are more than 2000 such points involved in the calculation every day.  
+5.There are many models involved in kriging algorithm, we use linear semivariogram function.  
+5.Our project is based on the open source kriging algorithm in `1` and the code provided by Agilor.  
+# Environment
+## Quick start guide
+Prepare the environment  
+**1) Download the Poplar SDK**  
+
+Install the Poplar SDK following the instructions in the [Getting Started guide](https://docs.graphcore.ai/projects/ipu-pod-getting-started/en/latest/installation.html) for your IPU system. Make sure to source the enable.sh scripts for poplar.  
+
+**2) Python**
+
+Create a virtualenv and install the required packages:
+```
+bash
+virtualenv venv -p python3.6
+source venv/bin/activate
+pip install <path to the tensorflow-1 wheel file from the Poplar SDK>
+pip install -r requirements.txt
+```
+# Run script
+Start the program by running `bash run.sh`, we use 1 day’s data to repeat 14 times to simulate 14 days of ET0 calculation process with 4 IPU, this will take 10 minutes.  
+# License
+The code presented here is licensed under the Apache License Version 2.0, see the LICENSE file in this directory.  
+`kriging_test.py` refers to `Pykrige`  
+`kriging_test.py` is licensed under BSD 3-Clause: https://github.com/GeoStat-Framework/PyKrige/blob/main/LICENSE  
---
+++ tensorflow/et0/run.sh
@@ -0,0 +1,88 @@
+# Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file has been modified by Graphcore Ltd.
+
+so_file="matrix_solve/matrix_solve_ce_op/libmatrix_solve_ce_op.so"
+gp_file="matrix_solve/matrix_solve_ce_op/matrix_solve_codelets.gp"
+save_folder="test_result"
+if [ ! -f "$so_file" ] || [ ! -f "$gp_file" ]; then
+ cd matrix_solve/matrix_solve_ce_op
+ make
+ cd ../../
+fi
+if [ ! -x "$save_folder" ]; then
+ mkdir "$save_folder"
+fi
+
+START_TIME=`date +%s`
+file_root="test_chinese_map/"
+prs_file="raw_data/SURF_CLI_CHN_MUL_DAY-PRS-10004-20210403.TXT"
+rhu_file="raw_data/SURF_CLI_CHN_MUL_DAY-RHU-13003-20210403.TXT"
+tem_file="raw_data/SURF_CLI_CHN_MUL_DAY-TEM-12001-20210403.TXT"
+win_file="raw_data/SURF_CLI_CHN_MUL_DAY-WIN-11002-20210403.TXT"
+file_dem="resource/DEM_china_1km.tif"
+file_latit="latitude/china_latitude.tif"
+tif_save_1=$save_folder"/ET0_generate_0103_cvt_total_1.tif"
+tif_save_2=$save_folder"/ET0_generate_0103_cvt_total_2.tif"
+tif_save_3=$save_folder"/ET0_generate_0103_cvt_total_3.tif"
+tif_save_4=$save_folder"/ET0_generate_0103_cvt_total_4.tif"
+tif_save_5=$save_folder"/ET0_generate_0103_cvt_total_5.tif"
+tif_save_6=$save_folder"/ET0_generate_0103_cvt_total_6.tif"
+tif_save_7=$save_folder"/ET0_generate_0103_cvt_total_7.tif"
+tif_save_8=$save_folder"/ET0_generate_0103_cvt_total_8.tif"
+tif_save_9=$save_folder"/ET0_generate_0103_cvt_total_9.tif"
+tif_save_10=$save_folder"/ET0_generate_0103_cvt_total_10.tif"
+tif_save_11=$save_folder"/ET0_generate_0103_cvt_total_11.tif"
+tif_save_12=$save_folder"/ET0_generate_0103_cvt_total_12.tif"
+tif_save_13=$save_folder"/ET0_generate_0103_cvt_total_13.tif"
+tif_save_14=$save_folder"/ET0_generate_0103_cvt_total_14.tif"
+
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_1 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_2 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_3 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_4                                                                                                                                                                   
+
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_5 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_6 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_7 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_8 
+
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_9 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_10 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_11 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_12 
+
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_13 & \
+python $file_root"kriging_test.py" --prs-file $prs_file --rhu-file $rhu_file --tem-file $tem_file --win-file $win_file \
+                                   --file-dem $file_dem --file-latit $file_latit --tif-save $tif_save_14
+
+
+END_TIME=`date +%s`
+ 
+EXECUTING_TIME=`expr $END_TIME - $START_TIME`
+UNIT=" seconds"
+echo "*********total cost**********: "$EXECUTING_TIME$UNIT
